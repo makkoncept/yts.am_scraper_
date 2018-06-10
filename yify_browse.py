d97223dd6e4979e88cb4f267c0dac3dea206971d
Mayank Nader <34679965+makkoncept@users.noreply.github.com>
2018-06-10 18:47:35 +0530

    scrape torrent links

--- yify_browse.py
+++ yify_browse.py
@@ -5,7 +5,7 @@ import csv
 URL = "https://yts.am/browse-movies?page="
 csv_file = open('yify_movie_list.csv', 'w')
 csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['MOVIES', 'IMDB-RATING', 'NUMBER OF LIKES', 'NUMBER OF DOWNLOADS','IMDb Link'])
+csv_writer.writerow(['MOVIES', 'IMDB-RATING', 'NUMBER OF LIKES', 'NUMBER OF DOWNLOADS','IMDb Link','720p torrent','1080p torrent'])
 for page in range(1, 357):
     URL = "https://yts.am/browse-movies?page="+str(page)
     r = requests.get(URL).text
@@ -28,8 +28,14 @@ for page in range(1, 357):
             request = requests.get(movie_url).text
             n_soup = BeautifulSoup(request, "lxml")
             info = n_soup.find('div', class_="bottom-info")
+            torrent_info = n_soup.find('p', class_="hidden-xs hidden-sm")
             likes = info.find('span', id="movie-likes").text
             imdb_link = info.find('a', title="IMDb Rating")['href']
+            for torrent in torrent_info.findAll('a'):
+                if(torrent.text[:3] == "720"):         #according to yts, WEB = losslessly ripped from a streaming service, same quality as BluRay
+                    torrent_720 = torrent['href']
+                if(torrent.text[:4] == "1080"):
+                    torrent_1080 = torrent['href']
             downladed = n_soup.find('div', id="synopsis")
             downloaded = downladed.find('p', class_=None)
             for down_loaded in downloaded.findAll('em'):
@@ -41,14 +47,18 @@ for page in range(1, 357):
             likes = None
             num_downloads = None
             imdb_link = None
+            torrent_720 = None
+            torrent_1080 = None
             pass
         movie_name = mov_name.a.text + " (" + movie_year + ")"
         print(movie_name)
         print("imdb rating:", rating)
         print("number of likes:", likes)
         print("number of downloads:", num_downloads)
-         print("IMDb link:", imdb_link)
-        csv_writer.writerow([movie_name, rating, likes, num_downloads, imdb_link])
+        print("IMDb link:", imdb_link)
+        print("720p torrent:", torrent_720)
+        print("1080p torrent:", torrent_1080)
+        csv_writer.writerow([movie_name, rating, likes, num_downloads, imdb_link, torrent_720, torrent_1080])
 
 print("done !!")
 csv_file.close()
