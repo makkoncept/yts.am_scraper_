1ee7ff1d30fff74ce0c8dd138bb469cad01d2b49
kiran s hombal <kiranhombal98@gmail.com>
2018-12-10 00:14:31 +0530

    Update downloadlinks.py

--- downloadlinks.py
+++ downloadlinks.py
@@ -1,6 +1,11 @@
 import csv
 import webbrowser
+import time
 with open('yify_movie_list.csv', 'rb') as open_file:
 	open_file.readline()
+	c = 0
 	for row in csv.reader(open_file):
 		webbrowser.open(row[6])
+		if(c%25 == 0):
+			time.sleep(10)
+		c = c + 1
